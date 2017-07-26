bd4e04c1821c967e6a0ac8f9288a5202ccd03c5c
Troy Nowicki <moe091@gmail.com>
2017-07-26 06:34:47 -0700

    disabled debug body

--- js/logic/effects.js
+++ js/logic/effects.js
@@ -11,7 +11,7 @@ BALL.effects = {
             BALL.effects.cur = BALL.gameState.special.create(parent.x + 120, parent.y, "k01-rocket");
             BALL.effects.cur.anchor.setTo(0.5);
 
-            game.physics.p2.enable(BALL.effects.cur, true);
+            game.physics.p2.enable(BALL.effects.cur, false);
             BALL.effects.cur.body.clearShapes();
             BALL.effects.cur.body.loadPolygon("newbods", "k01-rocket");
 
